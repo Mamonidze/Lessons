2b431b5d0f8f78692f50119c4d060c93b5d971b0
Mamon <dimonsmelov@gmail.com>
2025-08-20 20:14:31 +0300

    Changed RoundInt function. Added overloaded function

--- RoundToInt/RoundToInt.cpp
+++ RoundToInt/RoundToInt.cpp
@@ -3,32 +3,34 @@
  *для вещественных округляет и выводит округленное, для строк выводит “Ну и что вы от меня хотите”
  */
 
-void RoundToInt();
+void RoundToInt(const int& x);
+void RoundToInt(const double& x);
+void RoundToInt(const std::string& x);
 
 int main()
 {
-    RoundToInt();
+    const int a{5};
+    const double b{5.49};
+    const double c{5.51};
+    const std::string s{"test"};
+    
+    RoundToInt(a);
+    RoundToInt(b);
+    RoundToInt(c);
+    RoundToInt(s);
     return 0;
 }
 
-void RoundToInt()
+void RoundToInt(const int& x)
 {
-    float input;
-    std::cout << "Enter a value: ";
-    std::cin >> input;
-    if (std::cin.fail())
-    {
-        std::cout << "What do you want from me?";
-        return;
-    }
-    float result {input - static_cast<int>(input)};
-    if (result == 0.00)
-    {
-        std::cout << "It's already an integer";
-    }
-    else
-    if (result > 0.00)
-    {
-        std::cout << "Here is your rounded number: " << std::round(input) << '\n' ;
-    }
+    std::cout << "This is already an integer!" << '\n';
+}
+
+void RoundToInt(const double& x)
+{
+    std::cout << "Here is your rounded double: " << std::round(x) << '\n';
+}
+void RoundToInt(const std::string& x)
+{
+    std::cout << "What do you want from me?" << '\n';
 }
